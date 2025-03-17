ee36801e63165f939f7d8e360709d605c13dcd0c
Nhat Nam <mnnam.khtn@gmail.com>
2025-03-17 23:15:19 +0700

    FIX: Change port

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 require('dotenv').config();
 const app = require('./src/app')
 
-
-
-const {PORT} = process.env;
-
-
-
+const { PORT } = process.env || 3000;
 
 const server = app.listen( PORT, () => {
     console.log(`WSV start with port ${PORT}`);
